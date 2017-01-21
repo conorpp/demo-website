346521552dcc47d3a89e101656f4523da5866bac
Andrew <drewd8@vt.edu>
2017-01-20 21:32:12 -0500

    Add basic support for switching some device on and off.

--- web.py
+++ web.py
@@ -1,6 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
+LIGHT_STATE = False
+
+@app.route('/', methods = ['GET'])
+def home():
+    'Return a page with control widgets for all devices.'
+    return render_template(
+        'index.html',
+        light_state_on_click = 'Turn Off' if LIGHT_STATE else 'Turn On')
+
+@app.route('/update', methods = ['POST'])
+def update():
+    'Toggle the device state.'
+    global LIGHT_STATE
+    LIGHT_STATE = not LIGHT_STATE
+    if LIGHT_STATE:
+        print 'Light now on!'
+    else:
+        print 'Light now off!'
+    return redirect(url_for('home'))
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug = True)
+    
