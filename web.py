939b9dcf42eb29b7ed22bd34c829cb57a810872e
Andrew <drewd8@vt.edu>
2017-01-20 22:26:51 -0500

    added another switch to web and made it easier to add more

--- web.py
+++ web.py
@@ -1,29 +1,44 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, abort
 
 app = Flask(__name__)
 
-LIGHT_STATE = False
+class LightSwitch(object):
+    def turn_on(self):
+        print 'Light on!'
+    def turn_off(self):
+        print 'Light now off!'
+
+class FanSwitch(object):
+    def turn_on(self):
+        print 'Fan on!'
+    def turn_off(self):
+        print 'Fan off!'        
 
 @app.route('/', methods = ['GET'])
 def home():
     'Return a page with control widgets for all devices.'
-    return render_template(
-        'index.html',
-        light_state_on_click = 'Turn Off' if LIGHT_STATE else 'Turn On')
+    return render_template('index.html')
 
-@app.route('/update', methods = ['POST'])
-def update():
-    'Toggle the device state.'
-    global LIGHT_STATE
-    LIGHT_STATE = not LIGHT_STATE
-    if LIGHT_STATE:
-        ## Any code in this block will be run when the switch is turned on.
-        print 'Light now on!'
-    else:
-        ## Any code in this block will be run when the switch is turned off.
-        print 'Light now off!'
+@app.route('/turn_on/<device_name>', methods = ['POST'])
+def turn_on(device_name):
+    'Turn on the given device.'
+    if device_name not in DEVICES.keys():
+        abort(404)
+    DEVICES[device_name].turn_on()
+    return redirect(url_for('home'))
+
+@app.route('/turn_off/<device_name>', methods = ['POST'])
+def turn_off(device_name):
+    'Turn off the given device.'
+    if device_name not in DEVICES.keys():
+        abort(404)
+    DEVICES[device_name].turn_off()
     return redirect(url_for('home'))
 
 if __name__ == '__main__':
+    DEVICES = {
+        'light' : LightSwitch(),
+        'fan' : FanSwitch(),
+    }
     app.run(debug = True, host = '0.0.0.0')
     
